d57df9812c29b212f2574edc53c5bf67d16a2266
Daniel Brewer <danielbrewer2014@comcast.net>
2018-11-26 10:12:02 -0600

    first commit

---
+++ main.js
@@ -0,0 +1,14 @@
+// Create an object that represents your pet.
+
+// Name property with a string value.
+// Species property with a string value.
+// Nicknames property with an array value. Array contains strings.
+// Age property with an integer value
+
+let pet= {
+    name="Lily",
+    species="dog",
+    nicknames= ["LilyBelle","Lils"],
+    age= 10
+}
+
